b82f0929f78dcc7ab266dd28bc671dd3bbe5596f
Draylar <samuelhimes@gmail.com>
2019-06-11 19:25:37 -0500

    super everything fix

--- src/main/com/github/draylar/LineConverter.java
+++ src/main/com/github/draylar/LineConverter.java
@@ -2,6 +2,9 @@ package main.com.github.draylar;
 
 public class LineConverter
 {
+    // bone.cubeList.add(new ModelBox(bone, 0, 0, -4.0F, -47.0F, -9.0F, 8, 27, 18, 0.0F, false));
+    // bone.addBox( -4.0F, -27, -9, 8, 27, 18);
+
     /**
      * Convert the lines from a 1.12 Forge model class to a 1.14 Fabric model class with appropriate mapping changes.
      * @param string line to be converted
@@ -41,9 +44,27 @@ public class LineConverter
 
         if (string.contains(".cubeList.add(new ModelBox("))
         {
-            string = string.replaceAll("cubeList.add\\(new ModelBox\\(([^\\s]+), ([^\\s]+), ([^\\s]+) ", "addBox(");
-            string = string.replaceAll(", ([^\\s]+) ([^\\s]+)\\)\\);", ");");
-            return string;
+            string = string.replace("        ", "");
+
+            // start: body.cubeList.add(new ModelBox(body, 0, 16, -3.0F, 4.0F, -2.0F, 6, 10, 3, 0.0F, false));
+            String[] split = string.split(" ");
+            for(int i = 0; i < split.length; i++)
+            {
+                split[i] = split[i].replace(",", "");
+            }
+
+            String name = split[0].substring(0, split[0].indexOf("."));
+            int texX = Integer.parseInt(split[2]);
+            int texY = Integer.parseInt(split[3]);
+            float x = Float.parseFloat(split[4]);
+            float y = Float.parseFloat(split[5]);
+            float z = Float.parseFloat(split[6]);
+            int x1 = Integer.parseInt(split[7]);
+            int y1 = Integer.parseInt(split[8]);
+            int z1 = Integer.parseInt(split[9]);
+            float unknown = Float.parseFloat(split[10]);
+
+            return name + ".addBox(" + "\"" + name + "\"" + ", " + + x + "F" + ", " + y + "F" + ", " + z + "F" + ", "  + x1 + ", "  + y1 + ", "  + z1 + ", " + unknown + "F"  + ", "  + texX + ", "  + texY + ");";
         }
 
         if (string.contains("public void render"))
--- src/test/com/github/draylar/LineConverterTest.java
+++ src/test/com/github/draylar/LineConverterTest.java
@@ -12,12 +12,12 @@ class LineConverterTest
     @Test
     void testLineConversions()
     {
-        assertEquals("import net.minecraft.client.model.Cuboid;", CONVERTER.convertLine("import net.minecrat.client.model.ModelBox;"));
+        assertEquals("import net.minecraft.client.model.Cuboid;", CONVERTER.convertLine("import net.minecraft.client.model.ModelBox;"));
         assertEquals("import net.minecraft.client.render.entity.model.EntityModel;", CONVERTER.convertLine("import net.minecraft.client.model.ModelBase;"));
         assertEquals("", CONVERTER.convertLine("import org.lwjgl.opengl.GL11;"));
         assertEquals("private final Cuboid bone;", CONVERTER.convertLine("private final ModelRenderer bone;"));
         assertEquals("public class name extends EntityModel<YourEntity> {", CONVERTER.convertLine("public class name extends ModelBase {"));
-        assertEquals("bone.addBox(-1F, -1F, -1F, 2F, 2F, 2F);", CONVERTER.convertLine("bone.cubeList.add(new ModelBox(bone, 0, 0, -1F, -1F, -1F, 2F, 2F, 2F, 0, false));"));
+        assertEquals("body.addBox(-3.0F, 4.0F, -2.0F, 6, 10, 3, 0, 16, 0.0F);", CONVERTER.convertLine("body.cubeList.add(new ModelBox(body, 0, 16, -3.0F, 4.0F, -2.0F, 6, 10, 3, 0.0F, false));"));
         assertEquals("public void render(YourEntity entity, float f, float f1, float f2, float f3, float f4, float f5) {", CONVERTER.convertLine("public void render(Entity entity, float f, float f1, float f2, float f3, float f4, float f5) {"));
     }
 }
